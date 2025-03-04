0dcb468336f3ed7b80f4764ea123a560d443ab4a
Oscar-roqsoft <oscar.nnoje@gmail.com>
2025-03-03 20:13:38 -0800

    services

--- src/components/mainFooter.vue
+++ src/components/mainFooter.vue
@@ -69,6 +69,10 @@
                     Email: info@infinitepathways.uk 
                 </a></li>
                 <li><a href="mailto:contact.laboiserie@gmail.com" class="hover:text-gray-400">
+                    Call: +44 7876 210359 
+
+                </a></li>
+                <li><a href="mailto:contact.laboiserie@gmail.com" class="hover:text-gray-400">
                     WhatsApp: +44 7448 805912
                 </a></li>
               </ul>
